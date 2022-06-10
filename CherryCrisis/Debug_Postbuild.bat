cb916887cb70a6b21035381c5781e926906112b0
Lancelot Marechal <l.marechal@student.isartdigital.com>
2022-06-10 13:06:40 +0200

    [misc] Remove *Postbuild.bat, push dll directly in the right folder

--- CherryCrisis/Debug_Postbuild.bat
+++
@@ -1,3 +0,0 @@
-xcopy /s /y CherryEditor\lib\mono             x64\Debug\lib\mono\
-xcopy /s /y CherryEditor\lib\mono-source-libs x64\Debug\lib\mono-source-libs\
-xcopy /s /y dll\Externals					  x64\Debug\
